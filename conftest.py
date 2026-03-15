192a89c7a1fa7a3e39837736136bba8597948a65
Vikrant Ninave <92vikrantninave@gmail.com>
2026-03-15 23:49:38 +0530

    base code

--- conftest.py
+++ conftest.py
@@ -4,9 +4,10 @@ from selenium import webdriver
 @pytest.fixture
 def driver():
 
-    driver = webdriver.Chrome()
-
+    driver = webdriver.Firefox()
     driver.maximize_window()
+    #driver.implicitly_wait(5)
+    #driver.set_page_load_timeout(10)
     yield driver
 
     driver.quit()
