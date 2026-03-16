b661a37610095fd747e3f76f138075af66764cf4
Vikrant Ninave <92vikrantninave@gmail.com>
2026-03-16 13:06:24 +0530

    updated new files with ci

--- conftest.py
+++ conftest.py
@@ -6,8 +6,10 @@ def driver():
 
     driver = webdriver.Chrome()
     driver.maximize_window()
-    #driver.implicitly_wait(5)
+
+    driver.implicitly_wait(10)
     driver.set_page_load_timeout(30)
+
     yield driver
 
     driver.quit()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 def generate_login_data():
 
     test_cases = [
-        ("valid_user@gmail.com", "validpassword"),   # Positive
+        ("valid_user@gmail.com", "valid@123"),   # Positive
         ("invalid_user@gmail.com", "wrongpassword"), # Invalid credentials
         ("", "password123"),                         # Blank username
         ("user@gmail.com", "")                       # Blank password
